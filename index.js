36896b4dd763eccc1820d41be6c19ebc56dc07ec
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-01 21:23:15 +0530

    Added some more routes for practice

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
+app.get("/linkedin", (req, res) => {
+  res.send("https://linkedin.com/in/taraksh01");
+});
+
+app.get("/github", (req, res) => {
+  res.send("https://github.com/taraksh01");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("https://twitter.com/taraksh01");
+});
+
+app.get("/portfolio", (req, res) => {
+  res.send("https://taraksh01.vercel.app");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
